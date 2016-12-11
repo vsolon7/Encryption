f7c151de6cf4e8c292bd194b44319dc277cc3c87
vsolon7 <vsolon7@gmail.com>
2016-12-11 01:04:52 -0600

    Its 1 AM and heres what I think I added.. I'm not really thinking too straight lol
    So it will convert a string to binary (as of right now MUST be under the AESKey size you made. Will make it split the strings up later). Then XOR that string with the generated key and prints that out. I know this isn't AES encryption yet but I don't want to deal with the matrix yet lol.
    
    Good times

--- ConsoleApplication2/AES.h
+++ ConsoleApplication2/AES.h
@@ -18,7 +18,7 @@ class AESEncryption
 public:
 
 	//will load the map with all the values of converting numbers to b64
-	AESEncryption() 
+	AESEncryption()
 	{
 		for (int i = 0; i < 26; i++)
 		{
@@ -37,13 +37,25 @@ public:
 	}
 
 	//TODO
-	std::string encrypt(int key, std::string String)
+	std::vector<bool> encrypt(std::vector<bool> key, std::string String)
 	{
+		std::vector<bool> binStr(String.length() * 8 + 8);
+		binStr = stringToBinary(String);
 
+		int size = binStr.size();
+
+		std::vector<bool> encrypted(size);
+
+		for (int i = 0; i < size; i++)
+		{
+			encrypted[i] = binStr[i] ^ key[i];
+		}
+
+		return encrypted;
 	}
 
 	//TODO
-	std::string decrypt(int key, std::string String)
+	std::string decrypt(std::vector<bool> key, std::string String)
 	{
 
 	}
@@ -80,7 +92,7 @@ public:
 
 			then it does the exact same thing (converting binary to base64) but at the end blank groups of 6 become '=' not 'A'
 		*/
-		for ( int i = 0; ((((totalKeyBits) - i) % 3 == 0) && i < totalKeyBits)|| (totalKeyBits - i >= 8); i+=6)
+		for (int i = 0; ((((totalKeyBits)-i) % 3 == 0) && i < totalKeyBits) || (totalKeyBits - i >= 8); i += 6)
 		{
 			std::vector<bool> tempN(6);
 
@@ -101,14 +113,14 @@ public:
 		do
 		{
 			for (int i = 0; i < 8; i++)
-				intKey.push_back(0); 
+				intKey.push_back(0);
 
 		} while (intKey.size() % 6 != 0);
 
 		totalKeyBits = intKey.size();
 
 		//do the same thing as above basically
-		for (int i = track+6; i < (totalKeyBits - 6); i += 6)
+		for (int i = track + 6; i < (totalKeyBits - 6); i += 6)
 		{
 			std::vector<bool> tempN(6);
 
@@ -138,6 +150,110 @@ public:
 		}
 
 		return sum;
+	}
+
+	std::vector<bool> baseTenToBinary(int n)
+	{
+		std::vector<bool> binaryByte(8);
+
+		//can't be bigger than 256 its just gonna be a char
+		int binaryArray[8] = { 1, 2, 4, 8, 16, 32, 64, 128 };
+
+		//LOL HOLY FUCK. ill make this better really soon... for now just enjoy the beautiful if statements
+		while (n > 0)
+		{
+			if (n >= binaryArray[0])
+			{
+				if (n >= binaryArray[1])
+				{
+					if (n >= binaryArray[2])
+					{
+						if (n >= binaryArray[3])
+						{
+							if (n >= binaryArray[4])
+							{
+								if (n >= binaryArray[5])
+								{
+									if (n >= binaryArray[6])
+									{
+										if (n >= binaryArray[7])
+										{
+											n -= binaryArray[7];
+											binaryByte[0] = 1;
+										}
+										else
+										{
+											n -= binaryArray[6];
+											binaryByte[1] = 1;
+										}
+									}
+									else
+									{
+										n -= binaryArray[5];
+										binaryByte[2] = 1;
+									}
+								}
+								else
+								{
+									n -= binaryArray[4];
+									binaryByte[3] = 1;
+								}
+							}
+							else
+							{
+								n -= binaryArray[3];
+								binaryByte[4] = 1;
+							}
+						}
+						else
+						{
+							n -= binaryArray[2];
+							binaryByte[5] = 1;
+						}
+					}
+					else
+					{
+						n -= binaryArray[1];
+						binaryByte[6] = 1;
+					}
+				}
+				else
+				{
+					n -= binaryArray[0];
+					binaryByte[7] = 1;
+				}
+			}
+			else
+			{
+				n -= binaryArray[0];
+			}
+		}
+
+		return binaryByte;
+	}
+
+	std::vector<bool> stringToBinary(std::string s)
+	{
+		int sBitSize = s.length() * 8;
+		std::vector<bool> binary(sBitSize);
+
+		int index = 0;
+
+		for (unsigned int i = 0; i < sBitSize - 8; i += 8)
+		{
+
+			std::vector<bool> byteTemp(8);
+			byteTemp = baseTenToBinary(s[index]);
+
+			for (int j = i; j < i + 8; j++)
+			{
+				binary[j] = byteTemp[j - i];
+			}
+
+			index++;
+		}
+
+		return binary;
 	}
 
 	//My own power function. Enter the number followed by the power to raise it to
--- ConsoleApplication2/Source.cpp
+++ ConsoleApplication2/Source.cpp
@@ -5,12 +5,39 @@
 
 int main()
 {
-	std::string toEncrypt = "My name is Vincent";
+	std::string toEncrypt = "My name is Vinc";
+	//std::string encrypted;
 
 	AESEncryption Encryption;
 
-	for(int i = 1; i <= 100; i++)
-		std::cout << i << ".) "<< Encryption.keyToBase64(Encryption.generateKey(AES_128)) << "\n";
+	std::vector<bool> key = Encryption.generateKey(AES_128);
+	std::vector<bool> binaryString = Encryption.stringToBinary(toEncrypt);
+	std::vector<bool> encryptedBinary = Encryption.encrypt(key, toEncrypt);
+
+	
+
+	std::cout << "\n\nKey: " << Encryption.keyToBase64(key) << "\n\n";
+	std::cout << "Message: " << toEncrypt << "\n\n";
+
+	std::cout << "Binary: ";
+
+	for (int i = 0; i < binaryString.size(); i++)
+	{
+		std::cout << binaryString[i];
+
+		if ((i + 1) % 8 == 0)
+			std::cout << " ";
+	}
+
+	std::cout << "\n\nEncryptedBinary: ";
+
+	for (int i = 0; i < encryptedBinary.size(); i++)
+	{
+		std::cout << encryptedBinary[i];
+
+		if ((i + 1) % 8 == 0)
+			std::cout << " ";
+	}
 
 	system("pause");
 	return 0;
